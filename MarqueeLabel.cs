18bfe93c91fc9deb8733ef07a76bbcb5ff47e313
Stéphane Lenclud <github@lenclud.com>
2014-06-16 12:18:46 +0200

    Adding OwnTimer property to marquee. This enables us to use an external timer and thus use a single timer for multiple marquee.

--- MarqueeLabel.cs
+++ MarqueeLabel.cs
@@ -13,9 +13,7 @@ namespace SharpDisplayManager
 {
     class MarqueeLabel : Label
     {
-        private int CurrentPosition { get; set; }
-        private Timer Timer { get; set; }
-
+        private bool iOwnTimer;
 
         [Category("Behavior")]
         [Description("How fast is our text scrolling, in pixels per second.")]
@@ -23,35 +21,61 @@ namespace SharpDisplayManager
         [Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
         public int PixelsPerSecond { get; set; }
 
+        [Category("Behavior")]
+        [Description("Use an internal or an external timer.")]
+        [DefaultValue(true)]
+        [Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
+        public bool OwnTimer
+        {
+            get
+            {
+                return iOwnTimer;
+            }
+            set
+            {
+                iOwnTimer = value;
+
+                if (iOwnTimer)
+                {
+                    Timer = new Timer();
+                    Timer.Interval = 10;
+                    Timer.Tick += new EventHandler(Timer_Tick);
+                    Timer.Start();
+                }
+                else
+                {
+                    if (Timer != null)
+                        Timer.Dispose();
+                    Timer = null;
+                }
+
+            }
+        }
+        
+        private int CurrentPosition { get; set; }
+        private Timer Timer { get; set; }
         private DateTime LastTickTime { get; set; }
         private double PixelsLeft { get; set; }
         //DateTime a = new DateTime(2010, 05, 12, 13, 15, 00);
-//DateTime b = new DateTime(2010, 05, 12, 13, 45, 00);
-//Console.WriteLine(b.Subtract(a).TotalMinutes);
+        //DateTime b = new DateTime(2010, 05, 12, 13, 45, 00);
+        //Console.WriteLine(b.Subtract(a).TotalMinutes);
 
         public MarqueeLabel()
         {
             UseCompatibleTextRendering = true;
-            Timer = new Timer();
-            Timer.Interval = 10;
-            Timer.Tick += new EventHandler(Timer_Tick);
-            Timer.Start();
             //PixelsPerSecond = 32;
             LastTickTime = DateTime.Now;
             PixelsLeft = 0;
         }
 
-        void Timer_Tick(object sender, EventArgs e)
+        public void UpdateAnimation(DateTime aLastTickTime, DateTime aNewTickTime)
         {
             while (CurrentPosition > Width)
             {
                 CurrentPosition = -Width;
             }
 
-            DateTime NewTickTime=DateTime.Now;
-            PixelsLeft += NewTickTime.Subtract(LastTickTime).TotalSeconds * PixelsPerSecond;
-            LastTickTime = NewTickTime;
-            //offset += PixelsLeft;
+            PixelsLeft += aNewTickTime.Subtract(aLastTickTime).TotalSeconds * PixelsPerSecond;
 
             //Keep track of our pixels left over
             //PixelsLeft = offset - Math.Truncate(offset);
@@ -84,9 +108,15 @@ namespace SharpDisplayManager
             {
                 Invalidate();
             }
+        }
 
-
-
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime NewTickTime = DateTime.Now;                       
+            //
+            UpdateAnimation(LastTickTime, NewTickTime);
+            //
+            LastTickTime = NewTickTime;
         }
 
         protected override void OnPaint(PaintEventArgs e)
