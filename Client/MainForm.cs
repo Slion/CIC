006f9fa860cd9a7f6eb886ed7d77121ee6729172
Stéphane Lenclud <github@lenclud.com>
2014-08-12 20:55:50 +0200

    Adding our client implementation. Moving server into its own folder.

---
+++ Client/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using SharpDisplayManager;
+
+namespace SharpDisplayClient
+{
+    public partial class MainForm : Form
+    {
+        ChannelFactory<SharpDisplayManager.IDisplayService> iChannelFactory;
+        SharpDisplayManager.IDisplayService iClient;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSetText_Click(object sender, EventArgs e)
+        {
+            iClient.SetText(0,"Top");
+            iClient.SetText(1, "Bottom");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            iChannelFactory = new ChannelFactory<SharpDisplayManager.IDisplayService>(
+                new NetNamedPipeBinding(),
+                new EndpointAddress(
+                "net.pipe://localhost/DisplayService"));
+
+            iClient = iChannelFactory.CreateChannel();
+        }
+    }
+}
