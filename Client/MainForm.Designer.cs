f86d00a4a158c1d3fbd604b97a47944de2f61cb1
sl <sl@localhost>
2014-08-12 20:55:50 +0200

    Adding our client implementation. Moving server into its own folder.

---
+++ Client/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SharpDisplayClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSetText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonSetText
+            // 
+            this.buttonSetText.Location = new System.Drawing.Point(170, 104);
+            this.buttonSetText.Name = "buttonSetText";
+            this.buttonSetText.Size = new System.Drawing.Size(75, 23);
+            this.buttonSetText.TabIndex = 0;
+            this.buttonSetText.Text = "Set Text";
+            this.buttonSetText.UseVisualStyleBackColor = true;
+            this.buttonSetText.Click += new System.EventHandler(this.buttonSetText_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 252);
+            this.Controls.Add(this.buttonSetText);
+            this.Name = "MainForm";
+            this.Text = "Sharp Display Client";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSetText;
+    }
+}
+
