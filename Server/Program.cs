dbb8ffc1989f13672f52ea2bb6b6ba7eb0c3e6d6
sl <sl@localhost>
2015-01-18 12:35:22 +0100

    Auto Start should work.

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Principal;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace SharpDisplayManager
 {
@@ -16,7 +19,35 @@ namespace SharpDisplayManager
         /// </summary>
         [STAThread]
         static void Main()
-        {	
+        {
+
+			/*
+			if (!IsRunAsAdministrator())
+			{
+				var processInfo = new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase);
+
+				// The following properties run the new process as administrator
+				processInfo.UseShellExecute = true;
+				processInfo.Verb = "runas";
+
+				// Start the new process
+				try
+				{
+					Process.Start(processInfo);
+				}
+				catch (Exception)
+				{
+					// The user did not allow the application to run as administrator
+					MessageBox.Show("Sorry, this application must be run as Administrator.");
+				}
+
+				// Shut down the current process
+				Application.Exit();
+				return;
+				//Application.Current.Shutdown();
+			}*/
+
+
 			Application.ApplicationExit += new EventHandler(OnApplicationExit);
 			//
             Application.EnableVisualStyles();
@@ -24,6 +55,16 @@ namespace SharpDisplayManager
             iMainForm = new MainForm();
             Application.Run(iMainForm);
         }
+
+
+
+		static private bool IsRunAsAdministrator()
+		{
+		  var wi = WindowsIdentity.GetCurrent();
+		  var wp = new WindowsPrincipal(wi);
+
+		  return wp.IsInRole(WindowsBuiltInRole.Administrator);
+		}
 
 		/// <summary>
 		/// Occurs after form closing.
