d3a19d548f048944d2738d069539f675afeb3423
Stéphane Lenclud <github@lenclud.com>
2017-04-24 19:41:59 +0200

    Adding empty monitor configuration project.

---
+++ MonitorConfiguration/VirtualMonitor.h
@@ -0,0 +1,14 @@
+// MonitorConfiguration.h
+
+#pragma once
+
+using namespace System;
+
+namespace MonitorConfiguration
+{
+
+	public ref class VirtualMonitor
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
