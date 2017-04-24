2a0a462a205e77dc48e5692210ee2a9732a05f65
Stéphane Lenclud <github@lenclud.com>
2017-04-24 17:01:14 +0200

    Renaming to CIC.

--- Server/FormMain.Designer.cs
+++ Server/FormMain.Designer.cs
@@ -1302,7 +1302,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "FormMain";
-            this.Text = "Home Theatre Command & Information Center";
+            this.Text = "Command & Information Center";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.SizeChanged += new System.EventHandler(this.MainForm_SizeChanged);
